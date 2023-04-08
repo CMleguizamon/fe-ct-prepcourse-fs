623e314a83809746c8f1ff8e2a195b0413619cc6
Michelle <cmleguizamons@unal.edu.co>
2023-04-08 01:04:36 -0500

    Avance de tareas hasta modulo 4, CMLeguizamon

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -4,6 +4,7 @@
 function devolverString(string) {
    // Debe retornar un string.
    // Tu código:
+   return string;
 }
 
 // ⛔️ "X" e "Y" son números.
@@ -11,27 +12,40 @@ function devolverString(string) {
 function suma(x, y) {
    // Retorna el resultado de su suma.
    // Tu código:
+   return x+y; 
 }
 
 function resta(x, y) {
    // Retorna el resultado de la resta.
    // Tu código:
+   let a = x - y;
+   return a;
 }
 
 function divide(x, y) {
    // Retorna el resultado de su división.
    // Tu código:
+   return x/y;
 }
 
 function multiplica(x, y) {
    // Retorna el resultado de su multiplicación.
    // Tu código:
+   return x*y;
 }
 
 function obtenerResto(x, y) {
    // Obten el resto de la división de "x" entre "y".
    // Tu código:
+   return x%y;
 }
+
+devolverString("Hola");
+suma(1,2);
+resta(1,2);
+divide(25,2);
+multiplica(1,2);
+obtenerResto(1,2);
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
